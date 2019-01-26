2046c93564b2e79d26d7895c182521515db5b24d
Neketek <neketek@gmail.com>
2019-01-26 13:39:58 +0200

    Added reconnect behavior to server

--- server/server.py
+++ server/server.py
@@ -5,7 +5,9 @@ monkey.patch_all()
 def start():
     import time
     from slackclient import SlackClient
+    from slackclient.server import SlackConnectionError
     from modules.config.env import config as envconf
+    from modules.config import server as servconf
     from modules.controller.core import actions
     from modules.controller.scheduled.manager import Manager
     from modules.model import sql
@@ -19,11 +21,24 @@ def start():
         )
         Manager(client).start(threading.current_thread())
         while client.server.connected:
-            messages = client.rtm_read()
-            actions.feed(client, messages)
-            time.sleep(1)
-        else:
-            logger.error('Connection with Slack interrupted.')
+            try:
+                messages = client.rtm_read()
+                actions.feed(client, messages)
+                time.sleep(1)
+            except SlackConnectionError:
+                for i in range(servconf.RECONNECT_ATTEMPTS):
+                    logger.error(
+                        "Connection with slack was interrupted. "
+                        + "Trying to reconnect in {}s. Attempt:{}".format(
+                            servconf.RECONNECT_TIME_INTERVAL,
+                            i+1
+                        )
+                    )
+                    time.sleep(servconf.RECONNECTION_TIME_INTERVAL)
+                    if client.rtm_connect():
+                        break
+                else:
+                    break
     else:
         logger.error("Can't connect to Slack.")
 
